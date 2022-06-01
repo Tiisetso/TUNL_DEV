c6fd1104a6bd1555819d3098155bb87bfec1437f
Tiisetso Murray <tiisetso@tunl.to>
2022-06-01 06:47:47 +0000

    add code in files types: .ts,.json,.js remove code in files types: .ts,.json,.js

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("gvkpt")
 console.log("mjmxn")
-console.log("llkdc")
+console.log("pnggb")
--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
 console.log("lrdtv")
 console.log("fnicr")
 console.log("qpupm")
-console.log("fkjxv")
+console.log("uofxr")
